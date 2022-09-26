16b292b163f22f5ef6ac28f6954629e5640f773d
Sarasabbagh <42205895+Sarasabbagh@users.noreply.github.com>
2022-09-26 10:23:05 +0100

    added second test for green

--- color.test.js
+++ color.test.js
@@ -4,4 +4,7 @@ describe('color', () => {
   it('checks how many instances there are of each color and returns the numeric value for each', () =>{
     expect(color("Green, Green, Red, Amber, Red")).toEqual("Green:2, Amber:1, Red:2");
   });
+  it('counts the number of instances of Green in the string', () =>{
+    expect(color("Green, Green, Red, Amber, Red")).toInclude("Green:2");
+  });
 });
